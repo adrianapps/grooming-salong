d948bb8d7efde608369eeb5734c1558df3b03eb8
tempestpulse <bulsar@tutanota.com>
2024-11-19 15:25:32 +0100

    add services for each model

---
+++ frontend/src/app/services/service.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Service } from '../models/service.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ServiceService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getServices(): Observable<Service[]> {
+    return this.http.get<Service[]>(`${this.baseUrl}/services/`);
+  }
+
+  createService(serviceData: FormData): Observable<Service> {
+    return this.http.post<Service>(`${this.baseUrl}/services/`, serviceData);
+  }
+
+  getService(serviceId: number): Observable<Service> {
+    return this.http.get<Service>(`${this.baseUrl}/services/${serviceId}`);
+  }
+
+  updateService(serviceId: number, serviceData: FormData): Observable<Service> {
+    return this.http.put<Service>(
+      `${this.baseUrl}/services/${serviceId}`,
+      serviceData
+    );
+  }
+
+  deleteService(serviceId: number): Observable<Service> {
+    return this.http.delete<Service>(`${this.baseUrl}/services/${serviceId}`);
+  }
+}
---
+++ frontend/src/app/services/visit.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+import { Visit } from '../models/visit.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VisitService {
+  baseUrl = environment.apiUrl
+
+  constructor(private http: HttpClient) { }
+
+  getVisits(): Observable<Visit[]> {
+    return this.http.get<Visit[]>(`${this.baseUrl}/visits/`);
+  }
+
+  createVisit(visitData: FormData): Observable<Visit> {
+    return this.http.post<Visit>(`${this.baseUrl}/visits/`, visitData);
+  }
+
+  getVisit(visitId: number): Observable<Visit> {
+    return this.http.get<Visit>(`${this.baseUrl}/visits/${visitId}`);
+  }
+
+  updateVisit(visitId: number, visitData: FormData): Observable<Visit> {
+    return this.http.put<Visit>(`${this.baseUrl}/visits/${visitId}`, visitData);
+  }
+
+  deleteVisit(visitId: number): Observable<Visit> {
+    return this.http.delete<Visit>(`${this.baseUrl}/visits/${visitId}`);
+  }
+}
