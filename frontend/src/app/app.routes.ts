1f81c10f9d919a88a056160223ffe6dfdfc0a695
adrianapps <bulsar@tutanota.com>
2024-12-10 12:07:27 +0100

    remove service-form component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { ServiceFormComponent } from './service-form/service-form.component';
 import { DogFormComponent } from './dog-form/dog-form.component';
 import { VisitFormComponent } from './visit-form/visit-form.component';
 import { HomeComponent } from './home/home.component';
+import {ServiceTableComponent} from './service-table/service-table.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'add-service', component: ServiceFormComponent },
+    { path: 'services', component: ServiceTableComponent },
     { path: 'add-dog', component: DogFormComponent },
     { path: 'add-visit', component: VisitFormComponent }
 ];
